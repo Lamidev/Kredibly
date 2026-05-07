e724993f5dd50608c2125b4d339583c1dd912dfd
Thisslami <Akinmichaels18@gmail.com>
2026-05-07 18:21:59 +0100

    chore: final stability audit and identity lock

--- backend/controllers/business/businessController.js
+++ backend/controllers/business/businessController.js
@@ -4,6 +4,7 @@ const Waitlist = require("../../models/Waitlist");
 const { logActivity } = require("../../utils/activityLogger");
 const { getBanks, resolveAccount } = require("../../utils/nomba");
 const { getIO } = require("../../utils/socket");
+const User = require("../../models/User");
 
 const cleanPhone = (num) => {
     if (!num) return num;
@@ -384,7 +385,6 @@ exports.verifyKYC = async (req, res) => {
                         const accountName = resolvedData.account_name; // e.g. "SAMUEL OLAMIDE"
                         
                         // Get the User's registered name for comparison
-                        const User = require("../../models/User");
                         const user = await User.findById(req.user._id);
                         const registeredName = user?.name || profile.displayName;
 
